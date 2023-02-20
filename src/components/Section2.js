c80d39af802d8b2fd4262ceb932bf758690c2a2a
Mattsll99 <matthiassylla21@gmail.com>
2023-02-20 17:35:15 +0100

    up

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -6,7 +6,10 @@ function Section2() {
     <Container>
       <Wrapper>
         <Title>Charge Less, Earn More</Title>
-        <SubTitle>No intermediary between you and your customers</SubTitle>
+        <Wrap>
+          <Left>No intermediary between you and your customers</Left>
+          <Right>Almost no fee</Right>
+        </Wrap>
       </Wrapper>
     </Container>
   )
@@ -33,6 +36,7 @@ const Wrapper = styled.div`
   padding: 20px;
   display: flex; 
   flex-direction: column;
+  position: relative;
 `;
 
 const Title = styled.text`
@@ -47,3 +51,35 @@ const SubTitle = styled.text`
   color: #2E2E2E;
   margin-top: 30px;
 `;
+
+const Wrap = styled.div`
+  height: 70%; 
+  width: 100%;
+  position: absolute; 
+  bottom :0;
+  left:0; 
+  right: 0; 
+  margin-left: auto;
+  margin-right: auto;
+  background: transparent;
+  padding: 10px; 
+  display: flex; 
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  height: 100%; 
+  width: 48%;
+  background: #2E2E2E;
+  border-radius: 40px;
+  display: flex; 
+  color: #FFFFFF; 
+  font-weight: 200; 
+  font-size: 3vw;
+  align-items: center;
+  justify-content: center; 
+  text-align: center;
+`; 
+
+const Right = styled(Left)``;
